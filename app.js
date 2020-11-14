9ef0adfa531049cfc99b9c7e45d47e271db00639
Tore93 <totodibella93@gmail.com>
2020-11-14 17:25:23 +0100

    Angular + first sensor water temp

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var db = require('./database');
+//var db = require('./database');
 
 const constants = require('./constants');
 
